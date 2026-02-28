bc2564251d14082bb33c6ab447cc3d5b5f72f0ab
denise zahreddine <denise.zahreddine@gmail.com>
2026-02-28 20:50:35 +0100

    add flyway

--- backend/app/src/main/resources/db/migration/V2__test.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `flytest` (
-                                         `id_user` BIGINT NOT NULL AUTO_INCREMENT,
-                                         `first_name` VARCHAR(100) NOT NULL,
-                                         PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
